5f3e98a4d02e9b2314d8cc005a59e319336681be
KP <kp@cornerstonews>
2021-05-17 12:18:20 -0700

    Added preload file

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,53 @@
-import { app, BrowserWindow } from 'electron';
-declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-//   Add "electron-squirrel-startup" to dependency in package.json
-// if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-//   app.quit();
-// }
+declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 
-const createWindow = (): void => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    height: 800,
-    width: 1024,
-  });
+class ElectronMain {
+    public constructor() {
+        // This method will be called when Electron has finished
+        // initialization and is ready to create browser windows.
+        // Some APIs can only be used after this event occurs.
+        app.on("ready", () => this.createWindow());
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-  mainWindow.removeMenu();
+        // Quit when all windows are closed, except on macOS. There, it's common
+        // for applications and their menu bar to stay active until the user quits
+        // explicitly with Cmd + Q.
+        app.on("window-all-closed", () => {
+            if (process.platform !== "darwin") {
+                app.quit();
+            }
+        });
 
-  // Open the DevTools.
-//   mainWindow.webContents.openDevTools();
-};
+        app.on("activate", () => {
+            // On OS X it's common to re-create a window in the app when the
+            // dock icon is clicked and there are no other windows open.
+            if (BrowserWindow.getAllWindows().length === 0) {
+                this.createWindow();
+            }
+        });
+    }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+    public createWindow(): void {
+        // Create the browser window.
+        const mainWindow = new BrowserWindow({
+            height: 600,
+            width: 800,
+            webPreferences: {
+                preload: path.join(__dirname, "preload.js"),
+            },
+        });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+        // and load the index.html of the app.
+        //mainWindow.loadFile('index.html')
+        mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+        // Open the DevTools.
+        // mainWindow.webContents.openDevTools();
+    }
+}
+
+new ElectronMain();
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
